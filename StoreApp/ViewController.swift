6f3dc04daa8efc8b0ea0f44c3afecf854c199f77
Baresi0413 <baresirivas202@gmail.com>
2019-03-31 03:04:12 -0600

    Initial commit

---
+++ StoreApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TicTacToeLocal
+//
+//  Created by LABMAC26 on 08/03/19.
+//  Copyright © 2019 UTNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buSeletEvent(_ sender: Any) {
+        let buSelect = sender as! UIButton
+    }
+    
+}
+
